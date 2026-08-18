d5843b7e8dd794ab8a1d473217222e5d3087486b
Lucas Vinícius da Silva <lucasv2224@outlook.com>
2026-08-18 18:04:54 -0300

    initial commit

---
+++ src/main/java/com/agendadortarefas/AgendadortarefasApplication.java
@@ -0,0 +1,13 @@
+package com.agendadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadortarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadortarefasApplication.class, args);
+	}
+
+}
